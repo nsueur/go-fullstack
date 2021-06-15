830622333bce1bd65e73d22941e8a269b684160f
Nicolas <nicolas.sueur.pro@gmail.com>
2021-06-15 17:14:39 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+
+mongoose.connect('mmongodb://127.0.0.1:27017/',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+
+module.exports = app
